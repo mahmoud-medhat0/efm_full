c894d447a1113988d6674854bc5c70dcd7928f22
MahmoudWin11 <hhetlar120@gmail.com>
2024-10-29 08:47:38 +0300

    comment reset password

---
+++ public/build/assets/ProtectedRoute-CxM7I1wC.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-jHxTobjk.js";import{N as s}from"./index-CWV2AXI9.js";const m=({isAllowed:t,redircetPath:e,children:r,data:o})=>t?r:a.jsx(s,{to:e,state:o,replace:!0});export{m as default};
