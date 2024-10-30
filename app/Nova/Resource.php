abc261e6d34d5297f3c4e4dfa407b285d51061bd
MahmoudWin11 <hhetlar120@gmail.com>
2024-10-30 18:49:54 +0300

    resources permissions

--- app/Nova/Resource.php
+++ app/Nova/Resource.php
@@ -4,9 +4,20 @@ namespace App\Nova;
 
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Resource as NovaResource;
+use Illuminate\Http\Request;
 
 abstract class Resource extends NovaResource
 {
+    protected static $model;
+    protected static $className;
+
+    public static function getClassName()
+    {
+        $className = get_class(new static::$model);
+        $classNameParts = explode('\\', $className);
+        $lastPart = end($classNameParts);
+        return $lastPart;
+    }
     /**
      * Build an "index" query for the given resource.
      *
@@ -55,5 +66,29 @@ abstract class Resource extends NovaResource
     public static function relatableQuery(NovaRequest $request, $query)
     {
         return parent::relatableQuery($request, $query);
+    }
+    public static function authorizedToCreate(Request $request)
+    {
+        return auth('admin')->user()->can('create' . static::getClassName());
+    }
+    public function authorizedToUpdate(Request $request)
+    {
+        return auth('admin')->user()->can('update' . static::getClassName());
+    }
+    public function authorizedToDelete(Request $request)
+    {
+        return auth('admin')->user()->can('delete' . static::getClassName());
+    }
+    public function authorizedToView(Request $request)
+    {
+        return auth('admin')->user()->can('view' . static::getClassName());
+    }
+    public static function authorizedToViewAny(Request $request)
+    {
+        return auth('admin')->user()->can('viewAny' . static::getClassName());
+    }
+    public function authorizedToReplicate(Request $request)
+    {
+        return false;
     }
 }
