3ffad8a58db1f3c77de5aae474cece6cff72a460
MahmoudWin11 <hhetlar120@gmail.com>
2024-12-04 07:31:25 +0200

    add cancelled

--- nova-components/Depositsandwithdrawals/routes/api.php
+++ nova-components/Depositsandwithdrawals/routes/api.php
@@ -17,11 +17,11 @@ use App\Models\Transaction;
 Route::get('/get-deposits-and-withdrawals', function (Request $request) {
     $successDeposits = number_format(Transaction::where('type','deposit')->where('status','success')->sum('total'), 2);
     $pendingDeposits = number_format(Transaction::where('type','deposit')->where('status','pending')->sum('total'), 2);
-    $failedDeposits = number_format(Transaction::where('type','deposit')->where('status','canceled')->sum('total'), 2);
+    $failedDeposits = number_format(Transaction::where('type','deposit')->where('status','cancelled')->sum('total'), 2);
     $depositCharge = number_format(Transaction::where('type','deposit')->where('status','success')->sum('fee'), 2);
     $successWithdrawals = number_format(Transaction::where('type','withdraw')->where('status','success')->sum('total'), 2);
     $pendingWithdrawals = number_format(Transaction::where('type','withdraw')->where('status','pending')->sum('total'), 2);
-    $failedWithdrawals = number_format(Transaction::where('type','withdraw')->where('status','canceled')->sum('total'), 2);
+    $failedWithdrawals = number_format(Transaction::where('type','withdraw')->where('status','cancelled')->sum('total'), 2);
     $withdrawalCharge = number_format(Transaction::where('type','withdraw')->where('status','success')->sum('fee'), 2);
     return response()->json(['successDeposits' => $successDeposits, 'pendingDeposits' => $pendingDeposits, 'failedDeposits' => $failedDeposits, 'depositCharge' => $depositCharge, 'successWithdrawals' => $successWithdrawals, 'pendingWithdrawals' => $pendingWithdrawals, 'failedWithdrawals' => $failedWithdrawals, 'withdrawalCharge' => $withdrawalCharge]);
 });
