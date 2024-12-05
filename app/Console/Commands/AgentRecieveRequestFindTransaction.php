bdeb074b31964897ae72446c6dc69bc5629238fd
MahmoudWin11 <hhetlar120@gmail.com>
2024-12-05 20:28:39 +0200

    fix

--- app/Console/Commands/AgentRecieveRequestFindTransaction.php
+++ app/Console/Commands/AgentRecieveRequestFindTransaction.php
@@ -29,8 +29,8 @@ class AgentRecieveRequestFindTransaction extends Command
         $requests = AgentRecieveRequest::pending()->get();
         foreach($requests as $request){
             $transaction = Transaction::where('gateway_id',$request->gateway_id)->where('type','deposit')->where('status','pending')->where('total',$request->amount)->where('manual_fields','like','%"'.$request->sender_identifier.'"%')->first();
-            $this->info($request->id.' - '.$transaction->id);
             if($transaction){
+                $this->info($request->id.' - '.$transaction->id);
                 $request->status = 'approved';
                 $request->transaction_id = $transaction->id;
                 $request->save();
