2af2901b40ac9857573d0a4bcbc7fd51c495cb29
MahmoudWin11 <hhetlar120@gmail.com>
2024-12-17 03:37:45 +0200

    dd

---
+++ public/build/assets/ProtectedRoute-DCZMGoPK.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-DnwVPCvX.js";import{N as s}from"./index-Ct0xDUGx.js";const m=({isAllowed:t,redircetPath:e,children:r,data:o})=>t?r:a.jsx(s,{to:e,state:o,replace:!0});export{m as default};
