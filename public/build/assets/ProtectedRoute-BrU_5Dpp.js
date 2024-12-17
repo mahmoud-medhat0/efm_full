6b4f639014e68a4efabb568e7a3cb3731f4ea4ca
MahmoudWin11 <hhetlar120@gmail.com>
2024-12-17 03:35:24 +0200

    new iframe

---
+++ public/build/assets/ProtectedRoute-BrU_5Dpp.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-CRzte3xH.js";import{N as s}from"./index-CM4zV0W8.js";const m=({isAllowed:t,redircetPath:e,children:r,data:o})=>t?r:a.jsx(s,{to:e,state:o,replace:!0});export{m as default};
