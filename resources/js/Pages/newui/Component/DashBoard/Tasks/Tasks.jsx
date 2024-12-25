463aefb1f9778bbfe38d7f32c9e08b65eeda9364
MahmoudWin11 <hhetlar120@gmail.com>
2024-12-25 16:51:33 +0200

    s

--- resources/js/Pages/newui/Component/DashBoard/Tasks/Tasks.jsx
+++ resources/js/Pages/newui/Component/DashBoard/Tasks/Tasks.jsx
@@ -68,6 +68,7 @@ const Tasks = () => {
                 toast.error('VPN/Proxy Detected');
                 return;
             }
+            toast.success('Task is ' + response.data.ip_detected);
             return response.data.status;
         });
     };
